3e1b0544c1d46ecba1fe697dd9909edd54614cae
Zhaoting Chen <ztchenustc@gmail.com>
2024-04-28 23:29:49 +0100

    comments for sbatch script

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -6,20 +6,22 @@
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=16
 #SBATCH --mem-per-cpu=7G
-##SBATCH --nodes=1
-##SBATCH --ntasks-per-node=1
-##SBATCH --mem-per-cpu=7G
-##SBATCH --partition=Main
 #SBATCH --array=37-46
 
 SECONDS=0
 echo ">>> activate conda"
+# activate bash
 source /home/zchen/.bashrc
+# activate conda environment for 21cmfast and other things
 source activate sim
 module load cuda/11.8
 
+# in each realization
+mkdir rng_$SLURM_ARRAY_TASK_ID
 cd rng_$SLURM_ARRAY_TASK_ID
-#cd rng_44
+cp ../config_template.py config.py
+cp ../run21cmfast.py run21cmfast.py
+cp ../signalcov.py signalcov.py
 
 echo $PWD
 
@@ -27,15 +29,18 @@ echo ">>> run 21cmfast"
 
 python initialize.py
 
+# the loop size should correspond to the length of the step_list
 for i in {0..5..1}
 do 
 python run21cmfast.py faint $i HII_EFF_FACTOR
 python run21cmfast.py faint $i ION_Tvir_MIN
 done
+# run fiducial
 python run21cmfast.py faint 6 HII_EFF_FACTOR
+# calculate the covariances for Fisher analysis
 python signalcov.py faint
 
-
+# same but for the bright model
 for i in {0..5..1}
 do 
 python run21cmfast.py bright $i HII_EFF_FACTOR
